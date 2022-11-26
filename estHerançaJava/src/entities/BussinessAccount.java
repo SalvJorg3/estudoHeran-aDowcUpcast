cdde0e2c2de6df4477e8af231d26c477d094f51a
SalvJorg3 <jorgefer4816@gmail.com>
2022-11-26 17:04:36 -0300

    estudoHerançaJava

---
+++ estHerançaJava/src/entities/BussinessAccount.java
@@ -0,0 +1,39 @@
+package entities;
+
+/* Classe Business Account tem todos os dados e comportamentos da classe Account
+ todos os atributos e métodos */
+public class BussinessAccount extends Account {
+
+    private Double loanLimit;
+
+    public BussinessAccount() {
+    super();
+    }
+
+    public BussinessAccount(Integer number, String holder, Double balance, Double loanLimit) {
+        super(number, holder, balance);
+        this.loanLimit = loanLimit;
+    }
+
+    public Double getLoanLimit() {
+        return loanLimit;
+    }
+
+    public void setLoanLimit(Double loanLimit) {
+        this.loanLimit = loanLimit;
+    }
+
+    public void loan(double amount) {
+        if (amount <= loanLimit) {
+            balance += amount - 10.0;
+         }
+    }
+
+
+
+
+
+
+}
+
+
---
+++ estHerançaJava/src/entities/SavingsAccount.java
@@ -0,0 +1,27 @@
+package entities;
+
+public class SavingsAccount extends Account {
+
+    private Double interestedRate;
+    public SavingsAccount() {
+        super();
+    }
+    
+    public SavingsAccount(Integer number, String holder, Double balance, Double interestedRate) {
+        super(number, holder, balance);
+        this.interestedRate = interestedRate;
+   }
+
+   public Double getInterestedRate() {
+       return interestedRate;
+   }
+
+   public void setInterestedRate(Double interestedRate) {
+    this.interestedRate = interestedRate;
+   }
+
+   public void updateBalance() {
+    balance += balance * interestedRate;
+   }
+
+}
